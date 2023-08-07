1ac18e76cffbc35b5527e608a4e89830e68617c6
이연경 <inzidang@gmail.com>
2023-08-07 09:45:30 +0900

    123

--- src/sever.js
+++ src/sever.js
@@ -3,11 +3,19 @@ import "./db.js";
 import express from "express";
 import morgan from "morgan";
 import rentalRouter from "./routers/rentalRouter.js";
+import cors from "cors";
 
 // console.log(process.env.DB_URL);
 const PORT = 8080;
 const app = express();
 
+const corsOptions = {
+    origin: ["http://localhost:3000"],
+    methods: ["GET", "POST"],
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
